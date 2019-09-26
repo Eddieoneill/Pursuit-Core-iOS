49be79e4504cfa573528bd04ac2bf994e791e92e
Edward O'Neill <edwardoneill@Edwards-MacBook-Pro.local>
2019-09-26 12:52:10 -0400

    added more to playground

--- Unit1/Types and Variables.playground/Contents.swift
+++ Unit1/Types and Variables.playground/Contents.swift
@@ -1,5 +1,15 @@
 import UIKit
 
+// Swift
+/*
+ Swift was introduced in 2014
+ - swift is type safe language meening you can't mix  a declard data type with another e.g if var name = "Alex"
+ 
+   you cannot update name = 10 // won't comlile type String is not an Int
+ - moder language,
+ - provides optionals that allows us to know whether a variable has a value or not
+*/
+
 var str = "Hello, playground"
 
 // - comment
@@ -14,9 +24,33 @@ var str = "Hello, playground"
 // type Inferrence: implicit declaration of a varriable I
 var fellowName = "David Lin"
 
+print(fellowName) // Davit Lin
+
+fellowName = "Chelsi Christmas"
+
+print(fellowName) // Chelsi Christmas
+
+fellowName = "34"
+
+// DOES NOT COMPILE - CANNOT ADD DIFFERENT DATA TYPES
+//let newString = "34" + 12
+
+// concatenation - adding together
+let fullName = "Alex" + " Paul"
+
+// using String Interpo;atopm we are printing fullName's descrition
+print("Person's full name is \(fullName)")
+
 // Type Annotation - explicity writing the data type using : (colon)
 var cohort: String = "iOS 6.3"
 
+let lowercaseName = "alex"
+let uppercaseNames = "Alex"
+
+let stringResult = lowercaseName == uppercaseNames
+
+let alphabeticalOrder = "Kelyby" < "Lorraine"
+print("alphabeticalOrder result \(alphabeticalOrder)")
 //=======================================================
 //                      Character
 //=======================================================
@@ -42,6 +76,17 @@ futureAge
 // String interpolation is embedding a variable into a String using \(variavle name) syntax as seen belo
 print("Age of fellow 8 years from now is \(futureAge)")
 
+// integer division truncates decimals (removes them)
+let integerDivision = 16/3
+print("integer division is \(integerDivision)")
+
+
+//=======================================================
+//                   Division by 0
+//=======================================================
+
+// 89 / 0 DOES NOT COMPILE
+
 //=======================================================
 //                      Double
 //=======================================================
@@ -51,6 +96,30 @@ var balance = 100005.9
 balance = balance + 85_000
 
 print(balance)
+
+
+// floating point division keeps decimals
+
+let floatingPointDivision = 16.0 / 3.0
+print("floating point division is \(floatingPointDivision)")
+
+
+//=======================================================
+//                 Float vs Double
+// Float holds 32-bit vs 64-bit of a Double
+//=======================================================
+let floatNum: Float = 67.990776788943
+let doubleNum = 67.990776788943
+print("float is \(floatNum) and double is \(doubleNum)")
+
+// DOES NOT COMPILE - CANNOT ADD A FLOAT WITH A DOUBLE
+//let accountBalance = floatNum + doubleNum
+
+//here we are casting (converting) a Float to a Double
+let accountBalance = Double(floatNum) + doubleNum
+
+print("account balance is now \(accountBalance)")
+
 
 
 //=======================================================
